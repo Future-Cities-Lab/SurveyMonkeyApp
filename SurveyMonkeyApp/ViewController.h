1683c788e8c16c1b9265cbf65a48e8bc7b40e6ce
Collin Schupman <collinschupman@Collins-MacBook-Pro.local>
2016-10-20 10:30:34 -0700

    Initial Commit

---
+++ SurveyMonkeyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SurveyMonkeyApp
+//
+//  Created by Collin  Schupman on 10/20/16.
+//  Copyright © 2016 Collin  Schupman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SurveyMonkeyApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SurveyMonkeyApp
+//
+//  Created by Collin  Schupman on 10/20/16.
+//  Copyright © 2016 Collin  Schupman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
