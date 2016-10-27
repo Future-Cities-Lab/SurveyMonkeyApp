e71e3ce23121d5294ee383875c41980a32a18c43
Collin  Schupman <collinschupman@Collins-MacBook-Pro.local>
2016-10-27 11:12:01 -0700

    first commit

---
+++ SurveyMonkeyApp/SurveyMonkeyStateViewController.h
@@ -0,0 +1,13 @@
+//
+//  SurveyMonkeyStateViewController.h
+//  SurveyMonkeyApp
+//
+//  Created by Collin  Schupman on 10/20/16.
+//  Copyright © 2016 Collin  Schupman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SurveyMonkeyStateViewController : ViewController
+
+@end
---
+++ SurveyMonkeyApp/SurveyMonkeyStateViewController.m
@@ -0,0 +1,37 @@
+//
+//  SurveyMonkeyStateViewController.m
+//  SurveyMonkeyApp
+//
+//  Created by Collin  Schupman on 10/20/16.
+//  Copyright © 2016 Collin  Schupman. All rights reserved.
+//
+
+#import "SurveyMonkeyStateViewController.h"
+
+@interface SurveyMonkeyStateViewController ()
+
+@end
+
+@implementation SurveyMonkeyStateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
